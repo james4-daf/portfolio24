b4e8b0edcbfe91da527e65cee8a2ccd6c8719483
james4-daf <d.james@elsevier.com>
2023-12-29 23:23:40 +0000

    1

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dafydd James</h1>
+      <h1>Front End Web Dev at Elsevier</h1>
+      <h1>Years of Exp: 3.5</h1>
+      <h1>Tech: Js, React</h1>
+
+      <div className="projects">
+        <h1>Projects</h1>
+        <p>Gym Ecom</p>
+        <p>Gym Tracker</p>
+        <p>Js canvas racing game</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
